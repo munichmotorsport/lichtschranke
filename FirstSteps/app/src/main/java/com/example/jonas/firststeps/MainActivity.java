b696c3671e56ce466858ff473aae14dbcdfc7c46
nils inio <nils inio>
2015-12-15 00:11:12 +0100

    test

--- FirstSteps/app/src/main/java/com/example/jonas/firststeps/MainActivity.java
+++ FirstSteps/app/src/main/java/com/example/jonas/firststeps/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Listenspaß");
+        setTitle("Listenspaß als Test");
     }
 
     public void saveData(View view) {
