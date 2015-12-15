d2f367de69d63022b7d63a5ac0da08e57624ac46
Jonas Reitemeyer <jonas.reitemeyer@munichmotorsport.de>
2015-12-15 17:39:54 +0100

    created database

--- FirstSteps/app/src/main/java/com/example/jonas/firststeps/MainActivity.java
+++ FirstSteps/app/src/main/java/com/example/jonas/firststeps/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.jonas.firststeps;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> stringArray = new ArrayList<>();
@@ -19,10 +23,11 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Listenspaß");
     }
 
+
     public void saveData(View view) {
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        stringArray.add(message);
+        addData();
+        stringArray.clear();
+        stringArray.addAll(getData());
         ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stringArray);
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
@@ -31,9 +36,70 @@ public class MainActivity extends AppCompatActivity {
     public void deleteData(View view) {
         EditText editText = (EditText) findViewById(R.id.deleteText);
         String message = editText.getText().toString();
-        stringArray.remove(message);
-        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stringArray);
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+        deleteDataDB(message);
+        stringArray.clear();
+        if(!getData().isEmpty()) {
+            stringArray.addAll(getData());
+            ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stringArray);
+            ListView listView = (ListView) findViewById(R.id.listView);
+            listView.setAdapter(adapter);
+        }
     }
-}
+
+    public ArrayList<String> getData() {
+
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+
+        // Get the database. If it does not exist, this is where it will
+        // also be created.
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String selectQuery = "SELECT  * FROM " + DatabaseContract.Table1.TABLE_NAME;
+
+            Cursor cursor = db.rawQuery(selectQuery, null);
+            ArrayList<String> data = new ArrayList<>();
+            cursor.moveToFirst();
+                do {
+                    data.add(cursor.getString(1));
+                } while (cursor.moveToNext());
+            db.close();
+            return data;
+        }
+
+    public void addData() {
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+
+        // Get the database. If it does not exist, this is where it will
+        // also be created.
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Create insert entries
+        ContentValues values = new ContentValues();
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        values.put(DatabaseContract.Table1.COLUMN_NAME_COL1, message);
+
+
+        long newRowId = db.insert(
+                DatabaseContract.Table1.TABLE_NAME,
+                null,
+                values);
+
+        db.close();
+    }
+
+    public void deleteDataDB(String message) {
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+
+        // Get the database. If it does not exist, this is where it will
+        // also be created.
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+
+        db.delete(DatabaseContract.Table1.TABLE_NAME, DatabaseContract.Table1.COLUMN_NAME_COL1 + " = '"+ message +"'" , null);
+
+        db.close();
+    }
+
+    }
+
