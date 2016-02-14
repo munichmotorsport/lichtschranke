bf2a8057aaba369ff199cffbbdfb2a7bbbfc96f1
Jonas Reitemeyer <jonas.reitemeyer@munichmotorsport.de>
2016-02-14 19:16:40 +0100

    *added daofiles to build path

--- PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/CreateRace.java
+++ PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/CreateRace.java
@@ -1,23 +1,33 @@
 package munichmotorsport.photocellapplication;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-
 import java.util.ArrayList;
 
+import db.DaoMaster;
+import db.DaoSession;
+import db.Race;
+import db.RaceDao;
+
 public class CreateRace extends AppCompatActivity {
-    
+
+    RaceDao raceDao;
+    private SQLiteDatabase db;
+    private DaoMaster daoMaster;
+    private DaoSession daoSession;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_race);
         Spinner spinner = (Spinner) findViewById(R.id.modus_spinner);
         ArrayList<String> modi = new ArrayList<>();
-
         modi.add("Acceleration");
         modi.add("Auto Cross");
         modi.add("Endurance");
@@ -27,5 +37,13 @@ public class CreateRace extends AppCompatActivity {
         spinner.setAdapter(dropDown);
     }
 
+    public void createRace(View view){
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "photocell_db", null);
+        db = helper.getWritableDatabase();
+        daoMaster = new DaoMaster(db);
+        daoSession = daoMaster.newSession();
+        raceDao = daoSession.getRaceDao();
+        Race race = new Race(null, "testType", "testName");
 
+    }
 }
