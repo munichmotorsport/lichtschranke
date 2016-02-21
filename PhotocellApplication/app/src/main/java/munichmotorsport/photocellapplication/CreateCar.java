b8c49e2a7d8672e47aa4b4158ee4eb1a6123b1bd
nilsg <nilsgr>
2016-02-21 18:24:25 +0100

    Activity

--- PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/CreateCar.java
+++ PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/CreateCar.java
@@ -2,13 +2,29 @@ package munichmotorsport.photocellapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class CreateCar extends AppCompatActivity {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_car);
         setTitle("Neues Auto erstellen");
+
+        spinner = (Spinner) findViewById(R.id.teams);
+
+        ArrayList<String> teams = new ArrayList<>();
+        // TODO: load teams
+        // TODO: add teams to  teams
+        teams.add("municHMotorsport");  // Bsp:
+        ArrayAdapter<String> dropDown = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        dropDown.addAll(teams);
+        spinner.setAdapter(dropDown);
     }
 }
