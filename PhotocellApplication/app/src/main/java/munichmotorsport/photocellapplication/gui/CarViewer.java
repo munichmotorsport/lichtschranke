9c6247bcafab47e4b6627df165ccfa1b630ddd25
nilsgruenewald <nils.gruenewald@munichmotorsport.de>
2016-03-01 22:30:10 +0100

    views

---
+++ PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/gui/CarViewer.java
@@ -0,0 +1,15 @@
+package munichmotorsport.photocellapplication.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import munichmotorsport.photocellapplication.R;
+
+public class CarViewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_viewer);
+    }
+}
