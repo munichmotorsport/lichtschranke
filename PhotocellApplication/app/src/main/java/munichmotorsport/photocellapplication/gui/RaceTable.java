6143962e4143b632db8cf28f5473892b88180070
Jonas Reitemeyer <jonas.reitemeyer@munichmotorsport.de>
2016-03-15 20:05:04 +0100

    added table activity for showing lap times

---
+++ PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/gui/RaceTable.java
@@ -0,0 +1,15 @@
+package munichmotorsport.photocellapplication.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import munichmotorsport.photocellapplication.R;
+
+public class RaceTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_race_table);
+    }
+}
