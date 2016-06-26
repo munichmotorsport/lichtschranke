b98150fe122cd317008bf313162868630c72fb9f
nilsgruenewald <nils.gruenewald@munichmotorsport.de>
2016-06-26 21:35:36 +0200

    new Format

--- PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/utils/Data.java
+++ PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/utils/Data.java
@@ -5,8 +5,8 @@ public class Data {
     public static String url_pushCarName = "http://192.168.1.10:8080/mock/webapi/viewer/saveLap";
     public static String url_pushTime = "http://192.168.1.10:8080/mock/webapi/viewer/saveTime";
 //    public static String url_getLaps = "http://nilsgruenewald.de/webapi/getLastLap";
-//    public static String url_getLaps = "http://nilsgruenewald.de/webapi/viewer/saveLap";
-//    public static String url_getLaps = "http://nilsgruenewald.de/webapi/viewer/saveTime";
+//    public static String url_pushCarName = "http://nilsgruenewald.de/webapi/viewer/saveLap";
+//    public static String url_pushTime = "http://nilsgruenewald.de/webapi/viewer/saveTime";
 
     // colors in ViewCurrentRaceTable
     public static int textColor = 0x99000000;
