f099d4c18a8a4c005097c9f12feeee75a0488bd9
Jonas Reitemeyer <jonas.reitemeyer@munichmotorsport.de>
2016-06-26 20:55:22 +0200

    changed url

--- PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/utils/Data.java
+++ PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/utils/Data.java
@@ -1,8 +1,8 @@
 package munichmotorsport.photocellapplication.utils;
 
 public class Data {
-    //public static String url_getLaps = "http://192.168.1.10:8080/mock/webapi/getLastLap";
-    public static String url_getLaps = "http://nilsgruenewald.de/webapi/getLastLap";
+    public static String url_getLaps = "http://192.168.1.10:8080/mock/webapi/getLastLap";
+    //public static String url_getLaps = "http://nilsgruenewald.de/webapi/getLastLap";
 
     // colors in ViewCurrentRaceTable
     public static int textColor = 0x99000000;
