c47a1bae7e9e0d39085a2de3327c0439e29362fc
nilsgruenewald <nils.gruenewald@munichmotorsport.de>
2016-06-26 21:41:08 +0200

    merged

--- PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/utils/Data.java
+++ PhotocellApplication/app/src/main/java/munichmotorsport/photocellapplication/utils/Data.java
@@ -2,9 +2,6 @@ package munichmotorsport.photocellapplication.utils;
 
 public class Data {
     public static String url_getLaps = "http://192.168.1.10:8080/mock/webapi/getLastLap";
-
-    //public static String url_getLaps = "http://nilsgruenewald.de/webapi/getLastLap";
-
     public static String url_pushCarName = "http://192.168.1.10:8080/mock/webapi/viewer/saveLap";
     public static String url_pushTime = "http://192.168.1.10:8080/mock/webapi/viewer/saveTime";
 //    public static String url_getLaps = "http://nilsgruenewald.de/webapi/getLastLap";
